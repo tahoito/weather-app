2a295a993a07b90f5d4a2f502387d2989b67ea46
tahoito <taho.ito0304@gmail.com>
2026-01-03 17:10:17 +0900

    search-api終わり

--- backend/routes/web.php
+++ backend/routes/web.php
@@ -8,9 +8,3 @@ use App\Http\Controllers\Api\SpotSearchController;
 Route::get('/', function () {
     return view('welcome');
 });
-
-Route::get('/areas',[AreaController::class,'index']);
-Route::get('/spots/search',[SpotSearchController::class,'index']);
-Route::get('/favorites',[FavoriteController::class,'index']);
-Route::post('/favorites',[FavoriteController::class,'store']);
-Route::delete('/favorites/{spot}',[FavoriteController::class,'destroy']);
