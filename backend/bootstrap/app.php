9c0e27bcbfce73fa95d744969b6a437dcea751a5
tahoito <taho.ito0304@gmail.com>
2026-01-30 22:37:43 +0900

    edit

--- backend/bootstrap/app.php
+++ backend/bootstrap/app.php
@@ -1,7 +1,9 @@
 <?php
 
-use Illuminate\Foundation\Application;
+use Illuminate\Auth\AuthenticationException;
+use Illuminate\Http\Request;
 use Illuminate\Foundation\Configuration\Exceptions;
+use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Middleware;
 
 return Application::configure(basePath: dirname(__DIR__))
@@ -15,5 +17,13 @@ return Application::configure(basePath: dirname(__DIR__))
         //
     })
     ->withExceptions(function (Exceptions $exceptions): void {
-        //
+        $exceptions->render(function (AuthenticationException $e, Request $request) {
+            if ($request->is('api/*')) {
+                return response()->json([
+                    'message' => 'Unauthenticated',
+                ], 401);
+            }
+
+            return null;
+        });
     })->create();
